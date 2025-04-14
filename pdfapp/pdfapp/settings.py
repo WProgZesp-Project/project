aaaa364bfe0e50f98d472a09874273f9cb4dd159
bkajj <muuciek.new@gmail.com>
2025-04-14 23:25:20 +0200

    fixed project structure, hide private key

--- pdfapp/pdfapp/settings.py
+++ pdfapp/pdfapp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '54dw7y*jmx)xcj@f&bgz^k07psj2_p^n2+e+^(dy3p09(o9a=t'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pdfhandler', 
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'pdfapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'pdfhandler', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'pdfhandler', 'views')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,6 +81,8 @@ DATABASES = {
         'NAME': 'pdfdb',
     }
 }
+
+AUTH_USER_MODEL = 'pdfhandler.User'
 
 
 # Password validation
