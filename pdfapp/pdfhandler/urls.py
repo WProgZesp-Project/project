446aed2b3b76a1d28c3b7aaeae47e2af6557fd0e
wojciechp6 <wojciechp6@o2.pl>
2025-04-22 13:48:26 +0000

    Merge api & history

--- pdfapp/pdfhandler/urls.py
+++ pdfapp/pdfhandler/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views.index_view import index
+from .views.merge_pdf_view import merge_pdfs
 
 urlpatterns = [
     path('', index, name='index'),
+    path('api/merge-pdfs/', merge_pdfs, name='merge_pdfs'),
 ]
