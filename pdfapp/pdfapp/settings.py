86d2136fe18c2370b63cf06f23da0543a7b495f5
Kuba <jakub.wykocki@wp.pl>
2025-04-26 15:48:02 +0200

    change db to postgresql, adjust requirements

--- pdfapp/pdfapp/settings.py
+++ pdfapp/pdfapp/settings.py
@@ -77,11 +77,12 @@ WSGI_APPLICATION = 'pdfapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'pdfdb',
-        'CLIENT': {
-            'host': f"mongodb://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASSWORD')}@{os.getenv('MONGO_HOST')}:27017/pdfdb?authSource=admin",
-        }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
