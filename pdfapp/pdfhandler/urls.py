9c0050235be657b086badf675abf6b6710ffaf5b
Kuba <jakub.wykocki@wp.pl>
2025-04-26 17:03:10 +0200

    add user login

--- pdfapp/pdfhandler/urls.py
+++ pdfapp/pdfhandler/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from .views.index_view import index
 from .views.registration import UserRegistrationView, activate
+from .views.login import UserLoginView
 from .views.remove_password_view import remove_pdf_password, remove_password_page
 from .views.merge_pdf_view import merge_pdfs
 
 urlpatterns = [
     path('', index, name='index'),
     path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
     path('activate/<uidb64>/<token>', activate, name='activate'),
     path('remove-password/', remove_password_page, name='remove_password_page'),
     path('api/remove-password/', remove_pdf_password, name='remove_pdf_password'),
