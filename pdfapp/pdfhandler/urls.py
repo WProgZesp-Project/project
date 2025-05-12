2233fbf51a1ee39d4f5432de4adb160a07dfa223
mgarbula <mikolaj.garbula4@gmail.com>
2025-05-12 20:01:19 +0200

    extract pages from pdf file

--- pdfapp/pdfhandler/urls.py
+++ pdfapp/pdfhandler/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views.index_view import index
 from .views.registration import UserRegistrationView, activate
+from .views.merge_pdf_view import merge_pdfs
+from .views.extract_pdf_view import extract_pdf_pages
 
 urlpatterns = [
     path('', index, name='index'),
     path('register/', UserRegistrationView.as_view(), name='register'),
-    path('activate/<uidb64>/<token>', activate, name='activate')
+    path('activate/<uidb64>/<token>', activate, name='activate'),
     path('api/merge-pdfs/', merge_pdfs, name='merge_pdfs'),
+    path('api/extract-pages', extract_pdf_pages, name='extract-pages')
 ]
