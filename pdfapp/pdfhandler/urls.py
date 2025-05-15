279027c3f4dad1103e94f223d17c24fd59dbaef3
Maurice <maurycy095@gmail.com>
2025-05-15 19:13:54 +0200

    Add frontend and fundaments for backend (dashboard): before user-login merge

--- pdfapp/pdfhandler/urls.py
+++ pdfapp/pdfhandler/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views.index_view import index
 from .views.registration import UserRegistrationView, activate
+from .views.merge_pdf_view import merge_pdfs
+from .views.dashboard_view import dashboard
 
 urlpatterns = [
     path('', index, name='index'),
     path('register/', UserRegistrationView.as_view(), name='register'),
-    path('activate/<uidb64>/<token>', activate, name='activate')
+    path('activate/<uidb64>/<token>', activate, name='activate'),
     path('api/merge-pdfs/', merge_pdfs, name='merge_pdfs'),
+    path('dashboard/', dashboard, name='dashboard'),
 ]
