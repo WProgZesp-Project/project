8066a705625f891f433610663ba488a85047ffc7
ga1ile0 <dawid.wlosek@icloud.com>
2025-05-17 14:25:05 +0200

    fixing missing comma and import...

--- pdfapp/pdfhandler/urls.py
+++ pdfapp/pdfhandler/urls.py
@@ -3,6 +3,7 @@ from .views.index_view import index
 from .views.registration import UserRegistrationView, activate
 from .views.login import UserLoginView
 from .views.logout import UserLogoutView
+from .views.merge_pdf_view import merge_pdfs
 
 
 urlpatterns = [
@@ -10,6 +11,6 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
-    path('activate/<uidb64>/<token>', activate, name='activate')
+    path('activate/<uidb64>/<token>', activate, name='activate'),
     path('api/merge-pdfs/', merge_pdfs, name='merge_pdfs'),
 ]
