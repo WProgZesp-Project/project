bea204452150dd54aaa52beeabd05ba66f8e99ee
ga1ile0 <dawid.wlosek@icloud.com>
2025-05-17 15:11:49 +0200

    Finished log in, sign up, registration success pages

--- pdfapp/pdfhandler/urls.py
+++ pdfapp/pdfhandler/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views.index_view import index
-from .views.registration import UserRegistrationView, activate
+from .views.registration import UserRegistrationView, RegistrationSuccessView, activate
 from .views.login import UserLoginView
 from .views.logout import UserLogoutView
 from .views.merge_pdf_view import merge_pdfs
@@ -9,6 +9,7 @@ from .views.merge_pdf_view import merge_pdfs
 urlpatterns = [
     path('', index, name='index'),
     path('register/', UserRegistrationView.as_view(), name='register'),
+    path('register/success/', RegistrationSuccessView.as_view(), name='registration_success'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('activate/<uidb64>/<token>', activate, name='activate'),
