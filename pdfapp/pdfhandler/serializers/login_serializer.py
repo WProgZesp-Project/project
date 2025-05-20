1e8bc99fe587d6d8a43697a6b2822b0c6fc3578c
p3choco <62072811+p3choco@users.noreply.github.com>
2025-05-20 22:59:57 +0200

    code alligned to flake8 rules

--- pdfapp/pdfhandler/serializers/login_serializer.py
+++ pdfapp/pdfhandler/serializers/login_serializer.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class UserLoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
