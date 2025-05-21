57612af49127409286f6192caa9148c3067100c5
bkajj <muuciek.new@gmail.com>
2025-05-21 19:03:54 +0200

    Add frontend

--- pdfapp/pdfhandler/urls.py
+++ pdfapp/pdfhandler/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views.index_view import index
 from .views.registration import UserRegistrationView, activate
+from .views.operation_history_view import history_fragment, history_page
 
 urlpatterns = [
     path('', index, name='index'),
     path('register/', UserRegistrationView.as_view(), name='register'),
-    path('activate/<uidb64>/<token>', activate, name='activate')
+    path('activate/<uidb64>/<token>', activate, name='activate'),
     #path('api/merge-pdfs/', merge_pdfs, name='merge_pdfs'),
+    path('history/', history_page, name='history'),
+    path('history/fragment/', history_fragment, name='history_fragment'),
 ]
---
+++ pdfapp/pdfhandler/views/operation_history_view.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from ..models import OperationHistory
+
+def history_page(request):
+    return render(request, "history.html")
+
+def history_fragment(request):
+    if not request.user.is_authenticated:
+        return render(request, "partials/history_fragment.html", {"operations": []})
+    
+    operations = OperationHistory.objects.filter(
+        user=request.user,
+        operation_type='merge'
+    ).order_by('-created_at')[:5]
+
+    return render(request, "partials/history_fragment.html", {
+        "operations": operations
+    })
