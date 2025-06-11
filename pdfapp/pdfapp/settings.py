8247ea5c307b3f9168b7cf317c1a2683f6f8d99f
bkajj <muuciek.new@gmail.com>
2025-06-12 00:29:27 +0200

    allow aws host 3 test

--- pdfapp/pdfapp/settings.py
+++ pdfapp/pdfapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pdfapp-alb-431451943.eu-north-1.elb.amazonaws.com']
+ALLOWED_HOSTS = ['*', 'pdfapp-alb-431451943.eu-north-1.elb.amazonaws.com', "localhost"]
 
 
 # Application definition
