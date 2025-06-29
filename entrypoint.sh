317057931965518e6de9f3b7062ad91263c40dc1
bkajj <muuciek.new@gmail.com>
2025-06-30 00:38:38 +0200

    serve static

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,8 @@ cd /app/pdfapp
 echo "Running migrations..."
 python manage.py migrate
 
+echo "Setting static files..."
+python manage.py collectstatic --noinput
+
 echo "Starting Gunicorn..."
 exec gunicorn pdfapp.wsgi:application --bind 0.0.0.0:8000
--- pdfapp/pdfapp/settings.py
+++ pdfapp/pdfapp/settings.py
@@ -158,6 +158,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Emailing settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
