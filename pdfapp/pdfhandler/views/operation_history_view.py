36015893219ae1724fccbee8a4c918ff010cb82e
ga1ile0 <dawid.wlosek@icloud.com>
2025-06-29 18:17:32 +0200

    Fixed operation history visibility defect

--- pdfapp/pdfhandler/views/operation_history_view.py
+++ pdfapp/pdfhandler/views/operation_history_view.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from ..models import OperationHistory
 
 
+@login_required(login_url='login')
 def history_page(request):
     return render(request, "history.html")
 
 
+@login_required(login_url='login')
 def history_fragment(request):
-    if not request.user.is_authenticated:
-        return render(request, "partials/history_fragment.html", {"operations": []})
-
     operations = OperationHistory.objects.filter(
         user=request.user,
     ).order_by('-created_at')[:5]
