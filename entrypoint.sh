9ffd5d2613ae1de5139da3d2531bb13aa1b665a9
bkajj <muuciek.new@gmail.com>
2025-06-29 23:57:02 +0200

    gunicorn fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+export PYTHONPATH=/app
+
 echo "Running migrations..."
-python pdfapp/manage.py migrate --noinput
+python /app/pdfapp/manage.py migrate
 
 echo "Starting Gunicorn..."
 exec gunicorn pdfapp.wsgi:application --bind 0.0.0.0:8000
