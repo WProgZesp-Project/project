84ce0f21276169036fec5bd637061eb8844569fb
bkajj <muuciek.new@gmail.com>
2025-06-30 18:58:56 +0200

    dockerfile fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@
 # np. za pomocą wait-for-it.sh albo podobnego skryptu
 
 echo "Uruchamianie migracji bazy danych..."
-python pdfapp/manage.py migrate
+python manage.py migrate
 
 echo "Zbieranie statycznych plików..."
-python pdfapp/manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 echo "Uruchamianie serwera..."
 exec "$@"
